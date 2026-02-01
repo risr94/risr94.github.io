2a1f7747f5073757714c311a687964e9e0d5f905
RayRissling <raimund.rissling@hotmail.com>
2026-02-01 20:43:18 +0100

    v1.0.1 redone

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3968374977"
+    serviceWorkerVersion: "3154516747"
   }
 });
